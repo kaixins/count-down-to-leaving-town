6f680b35653742b3a1042df460691e3ef7cf08e5
kaixins <kaixin.shum@gmail.com>
2015-06-19 14:14:45 +0000

    Add initial styling

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -1,59 +1,77 @@
-if (Meteor.isClient) {
-    
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-    
+if(Meteor.isClient) {
+      
+    //Countdown
     var _second = 1000,
         _minute = _second * 60,
         _hour = _minute * 60,
         _day = _hour * 24;
-    
+
+
+    Meteor.startup(function() {
+        updateTimeLeft();
+        setInterval(function() {
+            updateTimeLeft();
+        }, 1000);
         
+    });
+    
     var getTimeLeft = function () {
-        var end = new Date('07/24/2015 16:00');
+        var end = new Date('08/01/2015 12:00');
         var now = new Date();
 
         return end - now;
     };   
-      
 
-   Template.countdown.helpers({
-     
-   getDays: function(){
-       var days = Math.floor(getTimeLeft() / _day);
-       return days;
-    },
-   getHours: function(){
-       var hours = Math.floor((getTimeLeft() % _day) / _hour);
-       return hours;
-    },
-    getMinutes: function(){
-       var minutes = Math.floor((getTimeLeft() % _hour) / _minute);
-       return minutes;
-    },
-    getSeconds: function(){
-       var seconds = Math.floor((getTimeLeft() % _minute) / _second);
-       return seconds;
-    }
-      
-  });
+     var updateTimeLeft = function() {
+         
+          var days = Math.floor(getTimeLeft() / _day);
+          Session.set('Days', days);
+         
+          var hours = Math.floor((getTimeLeft() % _day) / _hour);
+          Session.set('Hours', hours);
+         
+          var minutes = Math.floor((getTimeLeft() % _hour) / _minute);
+          Session.set('Minutes', minutes);
+         
+          var seconds = Math.floor((getTimeLeft() % _minute) / _second);
+         Session.set('Seconds', seconds);
+     };
+        
+    Template.countdown.helpers({
+        getDays: function() {
+            return Session.get("Days");
+        },
+        getHours: function() {
+            return Session.get("Hours");
+        },
+        getMinutes: function() {
+            return Session.get("Minutes");
+        },
+        getSeconds: function() {
+            return Session.get("Seconds");
+        }
+    });
     
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    //Animation
+    Template.sky.onRendered(function() {
+        var s = Snap('.sky');
+    
+        Snap.load('/images/cloud.svg', function (response) {
+            console.log(response);
+            console.log(s);
+            var cloud = response;
+            
+            s.append(cloud);
+        });
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer) {
+    Meteor.startup(function() {
+        
+    });
+    Meteor.methods({
+        
+    });
 }
